a100a3d3c7a65d2041473d0a1b95904ca616a057
ML31 <74205463+ML31@users.noreply.github.com>
2021-06-12 16:38:41 +0200

    Update main.jl

--- GeneticAlgorithm/main.jl
+++ GeneticAlgorithm/main.jl
@@ -1,28 +1,15 @@
-
-
 using JSON
 include("edit_distance.jl")
 include("GA.jl")
 
 
-problem = JSON.parsefile("C:/Users/basti/Documents/GitHub/PS_in_AI/instances/p2/p2_15_3-4.txt")
+problem = JSON.parsefile("C:/Users/basti/Documents/GitHub/PS_in_AI/instances/p2/p2_15_3-3.txt")
 
 
 alphabet = problem["alphabet"]
 strs = Vector{String}(problem["strings"])
 problem["str_length"]
 
-
-#Random.seed!(0)
-@time best = GeneticOptimization(alphabet, strs,
+best = GeneticOptimization(alphabet, strs,
     N_pop=150, max_length=30, n_max=150,
-    cre_p=1., dupl_p=0., mut_p=0., cross_p=0., warm_start=false)
-
-
-sum(edit_distance(best, s, zeros(Int,100,100)) for s in strs)
-length(best)
-
-
-using BenchmarkTools
-
-#@btime sum(edit_distance(best, s, zeros(Int,100,100)) for s in strs)
+    cre_p=0.23, dupl_p=0.2, mut_p=0.35, cross_p=0.23, warm_start=true)
