bedd7a6c57ac5e1033ba0baf46ee0a686beb2396
Markus Boeck <markus.boeck7800@hotmail.com>
2021-06-13 12:35:12 +0200

    make main.jl CLI

--- GeneticAlgorithm/main.jl
+++ GeneticAlgorithm/main.jl
@@ -1,28 +1,32 @@
 
 
 using JSON
+using Printf
 include("edit_distance.jl")
 include("GA.jl")
 
+println("Path to problem instance:")
+path = readline()
 
-problem = JSON.parsefile("C:/Users/basti/Documents/GitHub/PS_in_AI/instances/p2/p2_15_3-4.txt")
+problem = JSON.parsefile(path)
 
 
 alphabet = problem["alphabet"]
 strs = Vector{String}(problem["strings"])
 problem["str_length"]
+println("Problem:")
+println("Alphabet: $alphabet")
+println("Input strings:")
+for str in strs
+    println("\t", str)
+end
 
 
 #Random.seed!(0)
-@time best = GeneticOptimization(alphabet, strs,
+(best, ), t, = @timed GeneticOptimization(alphabet, strs,
     N_pop=150, max_length=30, n_max=150,
-    cre_p=1., dupl_p=0., mut_p=0., cross_p=0., warm_start=false)
+    cre_p=0.23, dupl_p=0.20, mut_p=0.35, cross_p=0.22, warm_start=true)
 
 
-sum(edit_distance(best, s, zeros(Int,100,100)) for s in strs)
-length(best)
-
-
-using BenchmarkTools
-
-#@btime sum(edit_distance(best, s, zeros(Int,100,100)) for s in strs)
+d = sum(edit_distance(best, s, zeros(Int,100,100)) for s in strs)
+println(@sprintf "Found %s with edit distance %d and length %d in %.2fs." best d length(best) t)
